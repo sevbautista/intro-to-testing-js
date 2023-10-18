7e904757c871202c4c6101b61c572cd01854d698
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-18 11:17:24 -0500

    intro-to-testing exercise 1

--- code.js
+++ code.js
@@ -1,4 +1,17 @@
 // helloWorld function 
 function helloWorld() {
-    return "Hello, World!";
+    return"Hello, World!";
 }
+//changing function name prevents it from being called, and causes all tests to fail, as it cannot pass the first test which is looking for helloWorld itself to be the defined function
+//if we changed the function name back to helloWorld the tests will pass, or if you changed the name of the function in the tests themselves, though this is not preferable.
+//changing the return to "Hello" causes the test of the return string to fail as it doesn't match. The other tests still pass, in that it is still a defined function, it does not return undefined, and it is returning a string.
+//changing to a console.log still defines the function but it no longer passes the string check, nor the name of the string, and the returns undefined check. (4)
+
+function isSeven(input) {
+    return input == 7;
+    }
+
+function sayHello() {
+
+}
+
--- tests.js
+++ tests.js
@@ -13,3 +13,27 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('isSeven', function(){
+    it('should be defined as a function', function() {
+        expect(typeof isSeven).toBe('function')
+    })
+    it('should return a boolean data type', function() {
+        expect (typeof isSeven()).toBe('boolean')
+    })
+    it('should return false if passed the number 5', function()  {
+        expect(isSeven(5)).toBe(false);
+    })
+    it('should return true if passed the number 7', function() {
+        expect(isSeven(7)).toBe(true);
+    })
+    it('should return true if passed "7"', function(){
+        expect(isSeven('7')).toBe(true);
+    })
+});
+
+describe('sayHello', function(){
+    it('should be defined as a function', function() {
+        expect(typeof sayHello).toBe('function');
+    })
+})
